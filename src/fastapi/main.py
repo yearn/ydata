5cd46a11af6ec44ff7f7eb8e9f78d34d892c1f99
borrito <yh01dlx@gmail.com>
2022-05-29 21:44:57 +0200

    feat: add api dir to routes

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -10,13 +10,13 @@ engine = create_engine(os.environ["DATABASE_URI"])
 app = FastAPI()
 
 
-@app.get("/")
-def redirect_root():
+@app.get("/api")
+def redirect_API_root():
     """Redirect to the OpenAPI documentation"""
     return RedirectResponse("/docs")
 
 
-@app.get("/vaults")
+@app.get("/api/vaults")
 def get_all_vaults():
     """Fetch vault-level risk metrics for all available vaults"""
     with Session(engine) as session:
@@ -25,7 +25,7 @@ def get_all_vaults():
     return {vault.address: json.loads(vault.info) for vault in vaults}
 
 
-@app.get("/vaults/{address}")
+@app.get("/api/vaults/{address}")
 def get_vault(address):
     """Fetch vault-level risk metrics for a specific vault"""
     with Session(engine) as session:
@@ -35,7 +35,7 @@ def get_vault(address):
     return json.loads(vault.info)
 
 
-@app.get("/strategies")
+@app.get("/api/strategies")
 def get_all_strategies():
     """Fetch strategy-level risk metrics for all available strategies"""
     with Session(engine) as session:
@@ -44,7 +44,7 @@ def get_all_strategies():
     return {strategy.address: json.loads(strategy.info) for strategy in strategies}
 
 
-@app.get("/strategies/{address}")
+@app.get("/api/strategies/{address}")
 def get_strategy(address):
     """Fetch strategy-level risk metrics for a specific strategy"""
     with Session(engine) as session:
