208ecd933c50f3a1df11b9d1b21f7783ed759f9d
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-08-16 09:17:44 +0800

    fix: Remove network and wallet connections;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,17 @@
-import React, { ReactElement } from 'react'
-import Link from 'next/link'
-import { AppProps } from 'next/app'
-import { Header } from '@yearn-finance/web-lib/layouts'
 import { WithYearn } from '@yearn-finance/web-lib/contexts'
-import { useBalance } from '@yearn-finance/web-lib/hooks'
+import { Header } from '@yearn-finance/web-lib/layouts'
 import LogoYearn from 'components/icons/LogoYearn'
-import Footer from 'components/StandardFooter'
 import Meta from 'components/Meta'
+import Footer from 'components/StandardFooter'
+import { AppProps } from 'next/app'
+import Link from 'next/link'
+import React, { ReactElement } from 'react'
 
 import '../style.css'
 
 function AppHeader(): ReactElement {
-  const [shouldDisplayPrice, set_shouldDisplayPrice] = React.useState(true)
-  const { data: YFIBalance } = useBalance({
-    for: '0x7a1057e6e9093da9c1d4c1d049609b6889fc4c67',
-    token: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
-  })
-
   return (
-    <Header>
+    <Header shouldUseNetworks={false} shouldUseWallets={false}>
       <div className={'flex-row-center w-full justify-between pr-4'}>
         <Link href={'/'}>
           <div className={'flex cursor-pointer flex-row items-center space-x-4'}>
@@ -26,15 +19,6 @@ function AppHeader(): ReactElement {
             <h1>{'Messari Frontier Web'}</h1>
           </div>
         </Link>
-        <div className={'hidden flex-row items-center space-x-6 md:flex'}>
-          <div className={'cursor-pointer'} onClick={(): void => set_shouldDisplayPrice(!shouldDisplayPrice)}>
-            {shouldDisplayPrice ? (
-              <p className={'text-primary-500'}>{`YFI $ ${YFIBalance.normalizedPrice}`}</p>
-            ) : (
-              <p className={'text-primary-500'}>{`Balance: ${YFIBalance.normalized} YFI`}</p>
-            )}
-          </div>
-        </div>
       </div>
     </Header>
   )
@@ -47,7 +31,7 @@ function MyApp(props: AppProps): ReactElement {
     <WithYearn
       options={{
         web3: {
-          shouldUseWallets: true,
+          shouldUseWallets: false,
           shouldUseStrictChainMode: false,
           defaultChainID: 1,
           supportedChainID: [1, 250, 42161, 1337, 31337],
